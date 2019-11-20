6b37638a81b11c218656749e0e12240d85dd1bd8
Nate Roundy <nroundy801@gmail.com>
2019-11-20 11:11:10 -0700

    Going to add bootstrap and sass

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
-import Login from "../Login-Register/Login"
-import Register from "../Login-Register/Register"
+import { Link } from 'react-router-dom'
 
 export class Landing extends Component {
   render() {
     return (
       <div>
         Landing.js
-        <Login />
-        <Register />
+
+
+        {/* <input
+          onChange={e => this.handleChange('email', e.target.value)}
+          value={this.state.email}
+          placeholder="Email"
+          type="text"
+        /> */}
+        <button>Login!</button>
+        <Link to="/login">
+        </Link>
+        
+        <Link to="/register">
+        <button>Register!</button>
+          <input placeholder="Username...." type="text"/>
+        </Link>
       </div>
     )
   }

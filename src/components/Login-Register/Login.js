3e5667c941bcf400d64826dc600650ba032e9d3b
Nate Roundy <nroundy801@gmail.com>
2019-11-20 22:30:58 -0700

    Login and Reg pages have are styled. Input fields plus btns added. Css @media added (now mobile friendly)

--- src/components/Login-Register/Login.js
+++ src/components/Login-Register/Login.js
@@ -1,13 +1,234 @@
-import React, { Component } from 'react'
+import React from "react";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { Link } from "react-router-dom";
+import {
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBMask,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+  MDBBtn,
+  MDBView,
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBFormInline,
+  MDBAnimation
+} from "mdbreact";
+import "./Login.css";
 
-export class Login extends Component {
+class Login extends React.Component {
+  state = {
+    collapseID: ""
+  };
+
+  toggleCollapse = collapseID => () =>
+    this.setState(prevState => ({
+      collapseID: prevState.collapseID !== collapseID ? collapseID : ""
+    }));
+
   render() {
+    const overlay = (
+      <div
+        id="sidenav-overlay"
+        style={{ backgroundColor: "transparent" }}
+        onClick={this.toggleCollapse("navbarCollapse")}
+      />
+    );
     return (
-      <div>
-        Login.js
+      <div id="classicformpage">
+        <div>
+          <MDBNavbar dark expand="md" fixed="top">
+            <MDBContainer>
+              <MDBNavbarBrand>
+                <strong className="white-text">MDB</strong>
+              </MDBNavbarBrand>
+              <MDBNavbarToggler
+                onClick={this.toggleCollapse("navbarCollapse")}
+              />
+              <MDBCollapse
+                id="navbarCollapse"
+                isOpen={this.state.collapseID}
+                navbar
+              >
+                <MDBNavbarNav left>
+                  <MDBNavItem active>
+                    <MDBNavLink to="#!">Home</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Link</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Profile</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+                <MDBNavbarNav right>
+                  <MDBNavItem>
+                    <MDBFormInline waves>
+                      <div className="md-form my-0">
+                        <input
+                          className="form-control mr-sm-2"
+                          type="text"
+                          placeholder="Search"
+                          aria-label="Search"
+                        />
+                      </div>
+                    </MDBFormInline>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBContainer>
+          </MDBNavbar>
+          {this.state.collapseID && overlay}
+        </div>
+
+        <MDBView>
+          <MDBMask className="d-flex justify-content-center align-items-center gradient">
+            <MDBContainer>
+              <MDBRow>
+                <MDBCol md="6" xl="5" className="mb-4">
+                  <MDBAnimation
+                    // type="fadeInRight"
+                    type="fadeInLeft"
+                    delay=".3s"
+                  >
+                    <MDBCard id="classic-card">
+                      <MDBCardBody className="white-text">
+                        <h3 className="text-center">
+                          <MDBIcon icon="user" /> Login:
+                        </h3>
+                        <hr className="hr-light" />
+                        <form action="">
+                        <MDBInput
+                          className="white-text"
+                          iconClass="white-text"
+                          label="Username"
+                          icon="user"
+                        />
+                        {/* <MDBInput
+                          className="white-text"
+                          iconClass="white-text"
+                          label="Your email"
+                          icon="envelope"
+                        /> */}
+                        <MDBInput
+                          className="white-text"
+                          iconClass="white-text"
+                          label="Your password"
+                          icon="lock"
+                          type="password"
+                          autoComplete="on"
+                        />
+                        </form>
+                        <div className="text-center mt-4 black-text">
+                          <MDBBtn color="indigo">Sign In</MDBBtn>
+                          <hr className="hr-light" />
+                          <div className="text-center d-flex justify-content-center white-label">
+                            <a href="#!" className="p-2 m-2">
+                              <MDBIcon
+                                fab
+                                icon="twitter"
+                                className="white-text"
+                              />
+                            </a>
+                            <a href="#!" className="p-2 m-2">
+                              <MDBIcon
+                                fab
+                                icon="linkedin"
+                                className="white-text"
+                              />
+                            </a>
+                            <a href="#!" className="p-2 m-2">
+                              <MDBIcon
+                                fab
+                                icon="instagram"
+                                className="white-text"
+                              />
+                            </a>
+                          </div>
+                        </div>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBAnimation>
+                </MDBCol>
+                <MDBAnimation
+                  // type="fadeInLeft"
+                  type="fadeInRight"
+                  delay=".3s"
+                  className="white-text text-center text-md-left col-md-6 mt-xl-5 mb-5"
+                >
+                  <h1 className="h1-responsive font-weight-bold">
+                    Don't have an account?
+                  </h1>
+                  <hr className="hr-light" />
+                  <h6 className="mb-4">
+                    <p>Beer Quote</p>
+                    Beer is proof that God loves us and wants us to be happy. ~
+                    Benjamin Franklin
+                  </h6>
+                  <Link to="/Register">
+                    <MDBBtn outline color="white">
+                      Register
+                    </MDBBtn>
+                  </Link>
+                </MDBAnimation>
+              </MDBRow>
+            </MDBContainer>
+          </MDBMask>
+        </MDBView>
       </div>
-    )
+    );
   }
 }
 
-export default Login
+export default Login;
+
+// import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+// import Button from "react-bootstrap/Button";
+
+// export class Landing extends Component {
+//   render() {
+//     return (
+//       <div>
+//         Landing.js
+//         {/* <input
+//           onChange={e => this.handleChange('email', e.target.value)}
+//           value={this.state.email}
+//           placeholder="Email"
+//           type="text"
+//         /> */}
+//         <Link to="/login">
+//           <Button variant="primary">Login</Button>
+//         </Link>
+//         <Link to="/register">
+//           <button>Register!</button>
+//           <input placeholder="Username...." type="text" />
+//         </Link>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Landing;
+
+// import React, { Component } from 'react'
+
+// export class Login extends Component {
+//   render() {
+//     return (
+//       <div>
+//         Login.js
+//       </div>
+//     )
+//   }
+// }
+
+// export default Login
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
-import Landing from './components/Landing/Landing'
+import Landing from './components/Login-Register/Register'
 import Login from './components/Login-Register/Login'
-import Register from './components/Login-Register/Register'
 import Dashboard from './components/Dashboard/Dashboard'
 import CreatePost from './components/Posting/CreatePost'
 import PostDetails from './components/Posting/PostDetails'
 
 export default (
   <Switch>
-    <Route exact path='/' component={Landing} />
-    <Route path='/login' component={Login} />
-    <Route path='/register' component={Register} />
+    <Route exact path='/' component={Login} />
+    <Route path='/register' component={Landing} />
     <Route path='/dashboard' component={Dashboard} />
     <Route path='/createpost' component={CreatePost} />
     <Route path='/postdetails' component={PostDetails} />
